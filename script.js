bc3df5530694bede64a807691bf0d885cdcfdc54
cessnaanh <cessnaanh@gmail.com>
2025-02-06 03:10:14 -0500

    updated html css and javascript

--- script.js
+++ script.js
@@ -6,3 +6,38 @@ var typed = new Typed('.text', {
     backSpeed: 50,
     loop: true,
 });
+
+// SHOW EDUCATION & EXPERIENCE
+let EducationBtn = document.querySelector('.education');
+let ExperienceBtn = document.querySelector('.experience');
+let show_box = document.querySelectorAll('.show_box');
+let hide_box = document.querySelectorAll('.hide_box');
+
+EducationBtn.addEventListener('click', () => {
+    show_box.forEach(Sbox => {
+        Sbox.style.opacity = '0';
+    });
+    hide_box.forEach(Hbox => {
+        Hbox.style.opacity = '1';
+    });
+});
+
+ExperienceBtn.addEventListener('click', () => {
+    show_box.forEach(Sbox => {
+        Sbox.style.opacity = '1';
+    });
+    hide_box.forEach(Hbox => {
+        Hbox.style.opacity = '0';
+    });
+});
+
+// SHOW MENU
+
+let bar = document.querySelector('.bars');
+let menu = document.querySelector('.menu');
+let nav = document.querySelector('.nav');
+
+bar.addEventListener('click', () => {
+    menu.classList.toggle('show_menu');
+    nav.classList.toggle('nav_border');
+});
